713c2fc4fc8effdb7b808d2cf44001bbc9f20fb4
asmitamitra <asmita.mitra130595@gmail.com>
2019-11-11 15:51:17 -0800

    Initial Commit

---
+++ MyMantra/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMantra
+//
+//  Created by Asmita Mitra on 11/11/19.
+//  Copyright © 2019 Asmita Mitra. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
